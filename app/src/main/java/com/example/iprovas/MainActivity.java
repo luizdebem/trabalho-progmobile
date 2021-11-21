1539bf8149ee0969b43841293d3747dde38b3d67
luizdebem <luizdebem99@gmail.com>
2021-11-21 16:47:54 -0300

    Create UA activity

--- app/src/main/java/com/example/iprovas/MainActivity.java
+++ app/src/main/java/com/example/iprovas/MainActivity.java
@@ -2,27 +2,35 @@ package com.example.iprovas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
   private ListView listView;
+  private FloatingActionButton fabAddUA;
 
   private void onItemClickListView(AdapterView<?> parent, View view, int position, long id) {
-    // Handle click
+    // @TODO Handle click UA
+  }
+
+  private void openCadastroUA(View view) {
+    Intent intent = new Intent(this, CadastroUA.class);
+    startActivity(intent);
   }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
     setTitle("iProvas - Listagem de UAs");
 
     String[] valores = new String[]{
@@ -40,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
       @Override
       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         onItemClickListView(parent, view, position, id);
+      }
+    });
+
+    fabAddUA = findViewById(R.id.fabAddUA);
+    fabAddUA.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        openCadastroUA(v);
       }
     });
   }
